e87059a3470a08210b38a8b76bf4eb273150d84f
Rob Curtis <surreymagpie@yahoo.co.uk>
2020-09-06 14:18:23 +0100

    Issue #2: Add JavaScript inline pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
-const { src, dest, series, watch } = require ('gulp');
+const { src, dest, series, parallel, watch } = require ('gulp');
 let sass = require('gulp-sass');
 sass.compiler = require ('node-sass');
+let concat = require('gulp-concat');
+
 let source = {
     sass: 'src/sass/**/*.s[a|c]ss',
+    js: 'src/js/**/*.js',
+}
+
+let destination = {
     css: 'src/site/_includes/assets/css',
-};
+    js: 'src/site/_includes/assets/js'
+}
 
 let styles = () => {
     return src(source.sass)
         .pipe(sass({outputStyle: "expanded" }).on('error', sass.logError))
-        .pipe(dest(source.css))
-};
+        .pipe(dest(destination.css));
+}
 
-let watchSass = (done) => {
+let scripts = () => {
+    return src(source.js)
+        .pipe(concat('main.js', {newLine: ''}))
+        .pipe(dest(destination.js));
+}
+
+let watchFiles = (done) => {
     watch(source.sass, styles);
+    watch(source.js, scripts);
     done;
 }
 
 exports.style = styles;
-exports.watch = watchSass;
-exports.default = series(styles, watchSass);
+exports.scripts = scripts;
+exports.watch = watchFiles;
+exports.default = series(parallel(styles, scripts), watchFiles);
